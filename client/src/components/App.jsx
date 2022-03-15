86aed3751ac0700b5ebd4791b74b81b55a688501
Justin Greer <justin.t.greer1@gmail.com>
2022-03-15 10:51:36 -0700

    Webpack setup!

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, createContext } from "react";
+// import React-DOM from 'react-dom';
+import axios from 'axios';
+export const AppContext = createContext();
+const API = '';
+
+const App = () => {
+
+  return (
+    <div>Jello World!</div>
+  )
+}
+
+export default App;
+
+//    <AppContext.Provider value={{ }}>
+// </AppContext.Provider>
